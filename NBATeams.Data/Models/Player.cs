8459e4bbc63fe445f202b6b9f69f5e75068f1d34
MartinCout <60389576+martincout@users.noreply.github.com>
2022-07-12 10:13:01 -0300

    Age method. Task-110

--- NBATeams.Data/Models/Player.cs
+++ NBATeams.Data/Models/Player.cs
@@ -15,8 +15,22 @@ namespace NBATeams.Data.Models
         public int ImageProfilePath { get; set; }
         public string Position { get; set; }
         public Stat Stats { get; set; }
-        public DateOnly BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public string Experience { get; set; } //Rookie, 2 year, 3 years.
         public Team Team { get; set; }
+
+        public int Age()
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - BirthDate.Year;
+            if (DateTime.Today < BirthDate.AddYears(age))
+            {
+                return --age;
+            }
+            else
+            {
+                return age;
+            }
+        }
     }
 }
