3318fe1d5a4ad154d7b7ebff477922d68defea4f
Agalegre <alegreagustn2501@gmail.com>
2022-07-12 14:47:10 -0300

    Creation models. Tasks 103-105-107

---
+++ Award.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Award
+{
+    public string Name { get; set; }
+
+    public int Year { get; set; }
+}
---
+++ Court.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Court
+{
+    public string Name { get; set; }
+
+    public string City { get; set; }
+}
