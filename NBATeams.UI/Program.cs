39ae1e4750854ba93579e98237fbd5a81f809fc8
MartinCout <60389576+martincout@users.noreply.github.com>
2022-07-18 15:51:43 -0300

    Added DbContext to services

--- NBATeams.UI/Program.cs
+++ NBATeams.UI/Program.cs
@@ -1,9 +1,10 @@
+using NBATeams.Data.Data;
 using NBATeams.Data.Repositories;
 using NBATeams.Domain.Services;
 using Petfy.Domain.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDbContext<NBATeamsDbContext>();
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
