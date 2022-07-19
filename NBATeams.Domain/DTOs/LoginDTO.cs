47ce4808e3e0e1850b7ec0b3b6c0de3256f715fb
MartinCout <60389576+martincout@users.noreply.github.com>
2022-07-19 16:06:48 -0300

    User's DTOs, register, login.

---
+++ NBATeams.Domain/DTOs/LoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Petfy.Domain.DTO
+{
+    public class LoginDTO
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
