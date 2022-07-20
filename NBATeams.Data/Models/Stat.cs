523b95c08dcb45883886961f5df868d2fb2079ea
Tobias Selva <tobiasselva1@gmail.com>
2022-07-20 00:16:52 -0300

    updating project

--- NBATeams.Data/Models/Stat.cs
+++ NBATeams.Data/Models/Stat.cs
@@ -11,6 +11,7 @@
         public decimal PIE { get; set; }
         public int Assists { get; set; }
         public int Points { get; set; }
+        public int average { get; set; }
 
         /// <summary>
         /// Calculates the average of all the values in order to qualify the Player by Its Stats and Scores.
