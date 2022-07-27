c07bff2b738c525b21383ee3fa4850a099ee25b8
MartinCout <martincoutinho00@gmail.com>
2022-07-27 11:19:42 -0300

    Changed Local,Visit type to Team - 210

--- NBATeams.Data/Models/Game.cs
+++ NBATeams.Data/Models/Game.cs
@@ -7,9 +7,9 @@ namespace NBATeams.Data.Models
     {
         public int Id { get; set; }
         [ForeignKey("LocalId")]
-        public OfficialTeam Local { get; set; }
+        public Team Local { get; set; }
         [ForeignKey("VisitId")]
-        public OfficialTeam Visit { get; set; }
+        public Team Visit { get; set; }
         public DateTime DateGame { get; set; }
         public int ScoreLocal { get; set; }
         public int ScoreVisit { get; set; }
