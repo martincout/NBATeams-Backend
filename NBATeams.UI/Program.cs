0a6575f2900411af54f61e4fdfa8cb61ea9dad8d
MartinCout <martincoutinho00@gmail.com>
2022-08-03 15:56:35 -0300

    Use routing

--- NBATeams.UI/Program.cs
+++ NBATeams.UI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using NBATeams.Data.Data;
@@ -51,6 +52,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
